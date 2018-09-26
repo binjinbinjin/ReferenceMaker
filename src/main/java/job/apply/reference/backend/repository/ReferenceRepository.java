1e790c3112e1a983156313349a89b60eb5701089
Jin Bin Liu <jinbinjinbin@live.ca>
2018-09-25 22:02:56 -0400

    add company search function in Reference repository

--- src/main/java/job/apply/reference/backend/repository/ReferenceRepository.java
+++ src/main/java/job/apply/reference/backend/repository/ReferenceRepository.java
@@ -1,8 +1,14 @@
 package job.apply.reference.backend.repository;
 
 import job.apply.reference.backend.domain.Reference;
+import job.apply.reference.backend.service.dto.ReferenceDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -11,5 +17,5 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ReferenceRepository extends JpaRepository<Reference, Long> {
-
+    Page<Reference> findAllByCompanyMatchesRegex(String regex, Pageable pageable);
 }
