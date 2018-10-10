5a5ca5ca3d4d15ffc6e469751ae373c1c63b6c6d
Jin Bin Liu <jinbinjinbin@live.ca>
2018-10-10 00:18:50 -0400

    Add Restful api to get the excel file form front end.

---
+++ src/main/java/job/apply/reference/backend/web/rest/ExcelResource.java
@@ -0,0 +1,46 @@
+package job.apply.reference.backend.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import job.apply.reference.backend.service.excelFileService.ExcelReaderService;
+import job.apply.reference.backend.service.dto.UploadResultDTO;
+import job.apply.reference.backend.service.impl.ReferenceServiceImpl;
+import job.apply.reference.backend.web.rest.errors.InternalServerErrorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("api/excel-document")
+public class ExcelResource {
+
+    private final Logger log = LoggerFactory.getLogger(ReferenceServiceImpl.class);
+
+    private final ExcelReaderService readExcelFile;
+
+    public ExcelResource(ExcelReaderService readExcelFile) {
+        this.readExcelFile = readExcelFile;
+    }
+
+    /**
+     * Post /upload?file=file a new excel file that contains the date that want to save into database
+     * @param  file a excel file
+     * @return the ResponseEntity with status 201 (Created) and with body the new referenceDTO
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     * */
+    @PostMapping("/upload")
+    @Timed
+    public ResponseEntity<UploadResultDTO> uploadExcelFile(@RequestParam("file") MultipartFile file) throws URISyntaxException {
+
+        this.log.debug("\n\n\n\n\ngetFile: {}",file.getSize() );
+        UploadResultDTO dto = this.readExcelFile.read(file);
+        if (dto == null) {
+            throw new InternalServerErrorException("UploadResultDTO cannot be null");
+        }
+        return  ResponseEntity.ok().body(dto);
+    }
+
+}
