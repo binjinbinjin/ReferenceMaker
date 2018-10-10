55ba3dae3d0fd207870995c7c5afa1fd769817d9
Jin Bin Liu <jinbinjinbin@live.ca>
2018-10-10 00:13:53 -0400

    Only the admin can access the "api/excel-document/**" uri.

--- src/main/java/job/apply/reference/backend/config/SecurityConfiguration.java
+++ src/main/java/job/apply/reference/backend/config/SecurityConfiguration.java
@@ -109,6 +109,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("api/excel-document/**").hasAuthority(AuthoritiesConstants.ADMIN)
         .and()
             .apply(securityConfigurerAdapter());
 
